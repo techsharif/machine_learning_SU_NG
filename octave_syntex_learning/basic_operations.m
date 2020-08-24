6cbdf05aac1b18489339ed88aad5259ba6fe3f8a
techsharif <sharif.cse.hstu@gmail.com>
2020-08-25 01:36:08 +0600

    octave basic operations

---
+++ octave_syntex_learning/basic_operations.m
@@ -0,0 +1,72 @@
+5+6
+3-2
+5*8
+
+1 == 2
+1 ~= 2
+
+1 && 2
+1 || 2
+xor(1, 0)
+
+
+a = 3
+
+a = 3;
+a
+
+b = 'hi'
+
+c = ( 3>= 1 )
+
+a = pi
+
+disp(a);
+
+disp(sprintf('%.02f\n', a));
+
+format long
+a
+
+% matrix 
+
+a = [1 2; 3 4; 5 6]
+
+a = [
+	1 2;
+	3 4;
+	5 6
+]
+
+v = [1 2 3]
+
+v = [1; 2; 3]
+
+v= 1:0.1:2 % generate vector
+
+v = 1:6
+
+ones(2,3)
+
+c = 2 * ones(2,3)
+
+ones(1,3)
+
+zeros(1, 3)
+
+rand(1,3)
+
+rand(3, 3)
+
+randn(1, 3)
+
+w = -6 + sqrt(10) * (randn(1, 10000));
+hist(w)
+hist(w, 50)
+
+eye(4)
+
+waitforbuttonpress ()
+
+
+
